f2488ecedfd0e7099a7a64338f321d1d8cb9b01f
NicolasVolkov <kroterup@gmail.com>
2026-08-14 16:02:02 -0300

    Adição do método transferir

--- princBanc.java
+++ princBanc.java
@@ -1,9 +1,16 @@
+package LPOO;
+
 public class princBanc {
     public static void main(String[] args) {
         ContaBancaria con= new ContaBancaria("Paulo", 34, 300);
+        ContaBancaria con2 = new ContaBancaria("José", 42, 310);
         con.depositar(34);
         con.sacar(200);
         con.mostrarSaldo();
+        con2.mostrarSaldo();
+        con.transferir(con2, 50);
+        con.mostrarSaldo();
+        con2.mostrarSaldo();
     }
 
 }
